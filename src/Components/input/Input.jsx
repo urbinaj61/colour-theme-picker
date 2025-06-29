359525bfdcc8e224f174f54a4a7b163ba6d9b710
Joseph <joseph.urbina61@hotmail.com>
2025-06-29 06:21:09 +0200

    changes applied

---
+++ src/Components/input/Input.jsx
@@ -0,0 +1,15 @@
+const Input = ({
+  autoficus,
+  required,
+  id,
+  className,
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+}) => {
+  return <input type="text" />;
+};
+
+export default Input;
